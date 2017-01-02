1e07718f2158f54bae0a382f5a961d44eb94dff2
Gary Thayer <garythayer@Garys-MacBook-Pro.local>
2017-01-02 15:31:25 -0500

    spelling error

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -12,10 +12,10 @@ const Filters = React.createClass({
 			<div className="monster-filters row col-sm-12">
 
 			<Helmet
-				title={ 'Monster Manager: Enounter Management Tools for GMs' }
+				title={ 'Monster Manager: Encounter Management Tools for GMs' }
 				meta={[
 					{name: "description", content: 'An interactive Dungeons and Dragons 5e Monster Manual featuring over 300 monsters from the D&D System Resource Documents (SRD).' },
-					{property: 'og:title', content: 'Monster Manager: Enounter Management Tools for GMs'},
+					{property: 'og:title', content: 'Monster Manager: Encounter Management Tools for GMs'},
 				]} />
 
 				<Filter {...this.props} value={this.props.filters.challenge_rating_min} key="challenge_rating_min" id="challenge_rating_min" filter="challenge_rating" label="Challenge Rating - Min"/>
