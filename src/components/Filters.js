e5d3986dc21b794a3cae914864400bf1d2c113c2
Gary Thayer <garythayer@Garys-MacBook-Pro.local>
2017-01-02 15:11:57 -0500

    main listing meta

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router';
 import Filter from './Filter';
+import Helmet from 'react-helmet';
 
 const Filters = React.createClass({
 
 	render() {
 
 		return (
+
 			<div className="monster-filters row col-sm-12">
+
+			<Helmet
+				title={ 'Monster Manager: Enounter Management Tools for GMs' }
+				meta={[
+					{name: "description", content: 'An interactive Dungeons and Dragons 5e Monster Manual featuring over 300 monsters from the D&D System Resource Documents (SRD).' },
+					{property: 'og:title', content: 'Monster Manager: Enounter Management Tools for GMs'},
+				]} />
 
 				<Filter {...this.props} value={this.props.filters.challenge_rating_min} key="challenge_rating_min" id="challenge_rating_min" filter="challenge_rating" label="Challenge Rating - Min"/>
 				<Filter {...this.props} value={this.props.filters.challenge_rating_max} key="challenge_rating_max" id="challenge_rating_max" filter="challenge_rating" label="Challenge Rating - Max"/>
