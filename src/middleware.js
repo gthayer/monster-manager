6d022ddacf6202172ac6a55fa04ee30e81844f76
Gary Thayer <garythayer@Garys-MacBook-Pro.local>
2017-01-16 22:06:05 -0500

    adsense snippet

--- src/middleware.js
+++ src/middleware.js
@@ -49,6 +49,14 @@ export default (req, res) => {
 						<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
 						<link rel='stylesheet' href='/bundle.css'>
+
+						<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+						<script>
+						  (adsbygoogle = window.adsbygoogle || []).push({
+						    google_ad_client: "ca-pub-7298702629619478",
+						    enable_page_level_ads: true
+						  });
+						</script>
 					</head>
 					<body>
 						<div id='app'>${renderedBody}</div>
