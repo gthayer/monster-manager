7c4b515d0666e0d492b93f68c138efc262655b5c
Gary Thayer <gary.thayer@get10up.com>
2021-03-31 01:00:49 -0400

    inititatives

--- src/reducers/simulation.js
+++ src/reducers/simulation.js
@@ -15,20 +15,6 @@ function simulation( state = [], action) {
 			const { party_a, party_b } = action;
 
 			let obj = new Simulation(party_a, party_b);
-			console.log( obj );
-
-			// // Give all creatures an initiative and sort them into parties.
-			// party_a.forEach(creature => {
-			// 	creature.party = 'A';
-			// 	creature.initiative = rollInitiative(creature);
-			// 	creatures.push(creature);
-			// });
-
-			// party_b.forEach(creature => {
-			// 	creature.party = 'B';
-			// 	creature.initiative = rollInitiative( creature );
-			// 	creatures.push(creature);
-			// });
 
 			// // Define the initiative order.
 			// creatures.sort( ( a,b ) => {
--- src/utils/simulation.js
+++ src/utils/simulation.js
@@ -2,12 +2,14 @@ class Simulation {
 
 	constructor( party_a, party_b ) {
 
-		this.party_a = [];
-		this.party_b = [];
+		this.party_a         = [];
+		this.party_b         = [];
+		this.initiativeOrder = [];
 
 		// Give all creatures an ID, initiative and sort them into parties.
 		party_a.forEach( ( creature, index ) => {
 			creature.party = 'A';
+			creature.hitpoints_max = creature.hit_points;
 			creature.encounterIndex = creature.party + index;
 			creature.initiative = this.rollInitiative(creature);
 
@@ -16,6 +18,7 @@ class Simulation {
 
 		party_b.forEach( ( creature, index ) => {
 			creature.party = 'B';
+			creature.hitpoints_max = creature.hit_points;
 			creature.encounterIndex = creature.party + index;
 			creature.initiative = this.rollInitiative(creature);
 
@@ -26,16 +29,42 @@ class Simulation {
 	}
 
 	run_encounter() {
+
+		this.sortInitiative();
+
 		console.log( this.party_b );
 	}
 
 	calculate_ability_mod( ability_score ) {
 		return Math.floor( ( ability_score / 2 )-5 );
 	}
+
+	sortInitiative() {
+		this.initiativeOrder.sort( ( a,b ) => {
+			if (a.initiative < b.initiative) return 1;
+			if (a.initiative > b.initiative) return -1;
+
+			const tiebreaker = this.rollDice(1,2);
+
+			if ( 1 === tiebreaker ) {
+				return 1;
+			} else {
+				return -1;
+			}
+		});
+	}
 	
 	rollInitiative( creature ) {
 		const dex_mod = this.calculate_ability_mod( creature.dexterity );
-		return this.rollDice( 1, 20, dex_mod );
+		const initiative = this.rollDice( 1, 20, dex_mod );
+
+		// Add the initiative to the queue.
+		var initiative_obj = {};
+		initiative_obj['encounterIndex'] = creature.encounterIndex;
+		initiative_obj['initiative']     = initiative;
+		this.initiativeOrder.push( initiative_obj );
+
+		return initiative;
 	}
 	
 	rollDice( qty = 1, sides = 6, mod = 0, crit = false, verbose = true ) {
